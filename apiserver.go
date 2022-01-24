1fa30f592f460b62bb0c10b798a0d52507459de0
skylerlee <skyler.ac.lee@gmail.com>
2022-01-24 22:53:00 +0800

    feat: update

--- apiserver.go
+++ apiserver.go
@@ -17,32 +17,32 @@ func APIServer() http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/upload", func(w http.ResponseWriter, r *http.Request) {
 		result := Result{}
+		defer json.NewEncoder(w).Encode(result)
 		cwd := r.FormValue("cwd")
 		name := r.FormValue("name")
 		file, _, err := r.FormFile("file")
+		if err != nil {
+			result = Result{Success: false, Error: "failed to parse form"}
+			return
+		}
 		// join destination file path
 		root, err := os.Getwd()
 		if err != nil {
-			result = Result{Success: false, Error: err.Error()}
+			result = Result{Success: false, Error: "failed to prepare file"}
 			return
 		}
 		destPath := filepath.Join(root, cwd, name)
-		if err != nil {
-			result = Result{Success: false, Error: err.Error()}
-			return
-		}
 		dest, err := os.OpenFile(destPath, os.O_WRONLY|os.O_CREATE, 0644)
 		if err != nil {
-			result = Result{Success: false, Error: err.Error()}
+			result = Result{Success: false, Error: "failed to read file"}
 			return
 		}
 		_, err = io.Copy(dest, file)
 		if err != nil {
-			result = Result{Success: false, Error: err.Error()}
+			result = Result{Success: false, Error: "failed to save file"}
 			return
 		}
 		result = Result{Success: true}
-		json.NewEncoder(w).Encode(result)
 	})
 	return mux
 }
