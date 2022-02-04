08844c2558ef38f488e6e12a1ec37bb66b3eb4ce
skylerlee <skyler.ac.lee@gmail.com>
2022-02-04 20:25:54 +0800

    feat: add get method

--- fsserver.go
+++ fsserver.go
@@ -2,6 +2,7 @@ package fsserver
 
 import (
 	"net/http"
+	"path"
 )
 
 func FSServer(root string) http.Handler {
@@ -19,4 +20,22 @@ type fsServer struct {
 	root string
 }
 
-func (s *fsServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {}
+func (s *fsServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		s.Get(w, r)
+	case http.MethodPost:
+		s.Post(w, r)
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
+func (s *fsServer) Get(w http.ResponseWriter, r *http.Request) {
+	rel := r.URL.Path
+	abs := path.Join(s.root, rel)
+	http.ServeFile(w, r, abs)
+}
+
+func (s *fsServer) Post(w http.ResponseWriter, r *http.Request) {
+}
